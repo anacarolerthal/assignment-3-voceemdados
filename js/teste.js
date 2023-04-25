a8dac5afd464324199d1e217ffbf3e07480aebe2
felipelmc <felipe.lamarca@hotmail.com>
2023-04-24 21:19:25 -0300

    Showing the map up

--- js/teste.js
+++ js/teste.js
@@ -6,7 +6,7 @@ var svg = d3.select("#viz2")
     .attr("width", width)
     .attr("height", height);
 
-d3.json("https://raw.githubusercontent.com/fgv-vis-2023/assignment-3-voceemdados/main/cidades.geojson?token=GHSAT0AAAAAAB66AVIDSXQDR6VW6YPB3MKSZCHDEDQ").then(function(data) {
+d3.json("https://raw.githubusercontent.com/felipelmc/Nascidos-Vivos-Viz/main/cidades.geojson").then(function(data) {
   var projection = d3.geoMercator()
       .scale(5000)
       .center([-43.1729, -22.9068])
